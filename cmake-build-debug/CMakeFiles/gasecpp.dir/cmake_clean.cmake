1d107bfdbdbe0b3b905e25c55b2831313a1e07fc
nhathm <nhathm@vng.com.vn>
2024-02-05 15:00:56 +0700

    chore: initialize project

---
+++ cmake-build-debug/CMakeFiles/gasecpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gasecpp.dir/library.cpp.obj"
+  "CMakeFiles/gasecpp.dir/library.cpp.obj.d"
+  "libgasecpp.dll"
+  "libgasecpp.dll.a"
+  "libgasecpp.dll.manifest"
+  "libgasecpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gasecpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef GASECPP_LIBRARY_H
+#define GASECPP_LIBRARY_H
+
+void hello();
+
+#endif //GASECPP_LIBRARY_H
